e16484e8d13256c306a0526690669d263c25ae63
filiashluch <filiasgamer@gmail.com>
2021-01-04 13:16:25 +0100

    v4

--- Documents/PlatformIO/Projects/projekt_final/src/main.cpp
+++ Documents/PlatformIO/Projects/projekt_final/src/main.cpp
@@ -1,22 +1,18 @@
-#include <Arduino.h>
 #include <WiFi.h>
 #include <ESPmDNS.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
 #include <WebSocketsServer.h>
+#include <DallasTemperature.h>
 #include "MAX30100_PulseOximeter.h"
 #include <OneWire.h>
-#include <DallasTemperature.h>
+#include <Wire.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
-#include <ESP32Ticker.h>
-
-#define DHTPIN 26
-#define LED1 32
-#define LED2 33
-#define MAX 19
-#define DHTTYPE DHT11
+#include <Ticker.h>
 #define DS18B20 19
+#define DHTPIN 25
+#define LED1 33
+#define LED2 32
+#define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 OneWire oneWire(DS18B20);
@@ -25,7 +21,7 @@ PulseOximeter pox;
 
 void send_sensor();
 
-Ticker blinker;
+Ticker timer;
 
 char webpage[] PROGMEM = R"=====(
 <!DOCTYPE html>
@@ -91,30 +87,25 @@ function send_data()
 </script>
 <body>
 <center>
-<h1>My Home Automation</h1>
+<h1>Systém pro monitorovaní zdravotního stavu pacienta</h1>
 <h3> LED 1 </h3>
 <button onclick= "button_1_on()" >On</button><button onclick="button_1_off()" >Off</button>
 <h3> LED 2 </h3>
 <button onclick="button_2_on()">On</button><button onclick="button_2_off()">Off</button>
 </center>
 <div style="text-align: center;">
-<h3>Temperature</h3><meter value="2" min="0" max="100" id="temp_meter"> </meter><h3 id="temp_value" style="display: inline-block;"> 2 </h3>
-<h3>Humidity</h3><meter value="2" min="0" max="100" id="hum_meter"> </meter><h3 id="hum_value" style="display: inline-block;"> 2 </h3>
-<h3>Humidity</h3><meter value="2" min="0" max="100" id="bpm_meter"> </meter><h3 id="bpm_value" style="display: inline-block;"> 2 </h3>
-<h3>Humidity</h3><meter value="2" min="0" max="100" id="SpO2_meter"> </meter><h3 id="SpO2_value" style="display: inline-block;"> 2 </h3>
-<h3>Humidity</h3><meter value="2" min="0" max="100" id="bodytemperature_meter"> </meter><h3 id="bodytemperature_value" style="display: inline-block;"> 2 </h3>
+<h3>Teplota</h3><meter value="2" min="0" max="100" id="temp_meter"> </meter><h3 id="temp_value" style="display: inline-block;"> 2 </h3>
+<h3>Vlhkost</h3><meter value="2" min="0" max="100" id="hum_meter"> </meter><h3 id="hum_value" style="display: inline-block;"> 2 </h3>
+<h3>Tep</h3><meter value="2" min="0" max="100" id="bpm_meter"> </meter><h3 id="bpm_value" style="display: inline-block;"> 2 </h3>
+<h3>Kyslík</h3><meter value="2" min="0" max="100" id="SpO2_meter"> </meter><h3 id="SpO2_value" style="display: inline-block;"> 2 </h3>
+<h3>Teplota těla</h3><meter value="2" min="0" max="100" id="bodytemperature_meter"> </meter><h3 id="bodytemperature_value" style="display: inline-block;"> 2 </h3>
 </body>
 </html>
 )=====";
 
-// ipaddress/led1/on
-//ipaddress/led1/off
-
-// ipaddress/led2/on
-//ipaddress/led2/off
 #include <ESPAsyncWebServer.h>
 
-AsyncWebServer server(80); // server port 80
+AsyncWebServer server(80);
 WebSocketsServer websockets(81);
 
 void notFound(AsyncWebServerRequest *request)
@@ -122,44 +113,38 @@ void notFound(AsyncWebServerRequest *request)
   request->send(404, "text/plain", "Page Not found");
 }
 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-
-  switch (type) 
+  switch (type)
   {
-    case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", num);
-      break;
-    case WStype_CONNECTED: {
-        IPAddress ip = websockets.remoteIP(num);
-        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-      }
-      break;
-    case WStype_TEXT:
-      Serial.printf("[%u] get Text: %s\n", num, payload);
-      String message = String((char*)( payload));
-      Serial.println(message);
-
-      
-     DynamicJsonDocument doc(200);
-    // deserialize the data
-    DeserializationError error = deserializeJson(doc, message);
-    // parse the parameters we expect to receive (TO-DO: error handling)
-      // Test if parsing succeeds.
-  if (error) {
-    Serial.print("deserializeJson() failed: ");
-    Serial.println(error.c_str());
-    return;
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Odpojeno!\n", num);
+    break;
+  case WStype_CONNECTED:
+  {
+    IPAddress ip = websockets.remoteIP(num);
+    Serial.printf("[%u] Připojeno z: %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
   }
+  break;
+  case WStype_TEXT:
+    Serial.printf("[%u] get Text: %s\n", num, payload);
+    String message = String((char *)(payload));
+    Serial.println(message);
 
-  int LED1_status = doc["LED1"];
-  int LED2_status = doc["LED2"];
-  digitalWrite(LED1,LED1_status);
-  digitalWrite(LED2,LED2_status);
+    DynamicJsonDocument doc(200);
+    DeserializationError error = deserializeJson(doc, message);
+    if (error)
+    {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
 
-
-
-
+    int LED1_status = doc["LED1"];
+    int LED2_status = doc["LED2"];
+    digitalWrite(LED1, LED1_status);
+    digitalWrite(LED2, LED2_status);
   }
 }
 
@@ -168,89 +153,92 @@ void onBeatDetected()
   Serial.println("Beat!");
 }
 
-void send_sensor()
-{
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  float BPM = pox.getHeartRate();
-  float SpO2 = pox.getSpO2();
-  float bodytemperature = sensors.getTempCByIndex(0);
-  
-    if (isnan(h) || isnan(t) ) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-  // JSON_Data = {"temp":t,"hum":h}
-  String JSON_Data = "{\"temp\":";
-         JSON_Data += t;
-         JSON_Data += ",\"hum\":";
-         JSON_Data += h;
-         JSON_Data += ",\"tep\":";
-         JSON_Data += BPM;
-         JSON_Data += ",\"ox\":";
-         JSON_Data += SpO2;
-         JSON_Data += ",\"bodytemp\":";
-         JSON_Data += bodytemperature;
-         JSON_Data += "}";
-   Serial.println(JSON_Data);     
-  websockets.broadcastTXT(JSON_Data);
-}
-
 void setup(void)
 {
-  
   Serial.begin(115200);
-  pinMode(MAX, OUTPUT);
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
   dht.begin();
-   if (!pox.begin()) {
-    Serial.println("FAILED");
-    for (;;);
-  } else {
-    Serial.println("SUCCESS");
-    pox.setOnBeatDetectedCallback(onBeatDetected);
-  }
- 
-   pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
-  
-  WiFi.softAP("techiesms", "");
+
+  WiFi.softAP("kontrola", "");
   Serial.println("softap");
   Serial.println("");
   Serial.println(WiFi.softAPIP());
 
-
-  if (MDNS.begin("ESP")) { //esp.local/
+  if (MDNS.begin("ESP"))
+  { //esp.local/
     Serial.println("MDNS responder started");
+
+    pinMode(19, OUTPUT);
+    Serial.print("Inicializace MAX30100..");
+
+    if (!pox.begin())
+    {
+      Serial.println("FAILED");
+      for (;;)
+        ;
+    }
+    else
+    {
+      Serial.println("SUCCESS");
+      pox.setOnBeatDetectedCallback(onBeatDetected);
+    }
+    pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
   }
 
-
-
-  server.on("/", [](AsyncWebServerRequest * request)
-  { 
-   
-  request->send_P(200, "text/html", webpage);
+  server.on("/", [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", webpage);
   });
 
-   server.on("/led1/on", HTTP_GET, [](AsyncWebServerRequest * request)
-  { 
-    digitalWrite(LED1,HIGH);
-  request->send_P(200, "text/html", webpage);
+  server.on("/led1/on", HTTP_GET, [](AsyncWebServerRequest *request) {
+    digitalWrite(LED1, HIGH);
+    request->send_P(200, "text/html", webpage);
   });
 
   server.onNotFound(notFound);
 
-  server.begin();  // it will start webserver
+  server.begin();
   websockets.begin();
   websockets.onEvent(webSocketEvent);
-  blinker.attach(2,send_sensor);
+  timer.attach(2, send_sensor);
 }
-
 
 void loop(void)
 {
- websockets.loop();
+  websockets.loop();
 }
 
+void send_sensor()
+{
+  pox.update();
+  sensors.requestTemperatures();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float BPM = pox.getHeartRate();
+  float SpO2 = pox.getSpO2();
+  float bodytemperature = sensors.getTempCByIndex(0);
+
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println(F("Nepodařilo se číst z DHT senzoru"));
+    return;
+  }
+  if (isnan(BPM) || isnan(SpO2))
+  {
+    Serial.println(F("Nepodařilo se číst z MAX30100 senzoru"));
+    return;
+  }
+  String JSON_Data = "{\"temp\":";
+  JSON_Data += t;
+  JSON_Data += ",\"hum\":";
+  JSON_Data += h;
+  JSON_Data += ",\"tep\":";
+  JSON_Data += BPM;
+  JSON_Data += ",\"ox\":";
+  JSON_Data += SpO2;
+  JSON_Data += ",\"bodytemp\":";
+  JSON_Data += bodytemperature;
+  JSON_Data += "}";
+  Serial.println(JSON_Data);
+  websockets.broadcastTXT(JSON_Data);
+}
